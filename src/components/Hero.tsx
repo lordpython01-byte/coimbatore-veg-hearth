a9ef0f1ec8e33398cf5211f7fc4c133b51aa3db6
lordpython01-byte <lordpython01@gmail.com>
2025-10-25 00:35:57 +0530

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
-import heroVideo from "@/src/assets/hero-video.mp4";
-
+import heroVideo from "@/assets/hero-video.mp4";
+ 
 const Hero = () => {
   const scrollToSection = (id: string) => {
     const element = document.getElementById(id);
